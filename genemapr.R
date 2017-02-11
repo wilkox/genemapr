5f4344a470a1223dbeb49c0f169f29affef874fb
David Wilkins <david@wilkox.org>
2017-02-11 18:19:19 +1100

    Working on plot.genemapr

--- genemapr.R
+++ genemapr.R
@@ -1,5 +1,6 @@
 library(grid)
 library(stringr) # TESTING ONLY
+library(magrittr) # TESTING ONLY
 
 # Draw a gene
 # Draws to the current viewport, in the native coordinate system
@@ -33,36 +34,8 @@ draw_gene <- function(Start, End, Fill) {
 # specified layout row and column
 draw_molecule <- function(Genes, Range = NULL, LayoutRow, LayoutCol) {
 
-  # Determine range for native coordinate system
-  if (is.null(Range)) {
-    Range <- c(min(c(Genes$Start), Genes$End), max(c(Genes$Start, Genes$End)))
-  } else if (length(Range) != 2) {
-    stop("Must specify range in form c(Min, Max)", call. = F)
-  } else if (!is.numeric(Range)) {
-    stop("Range must be numeric", call. = F)
-  }
-
-  # Create viewport for this molecule
-  MoleculeViewport <- viewport(
-    layout.pos.col = LayoutCol,
-    layout.pos.row = LayoutRow,
-    just = "bottom",
-    xscale = Range
-  ) %>%
-    pushViewport
-
   # Draw border
   grid.rect(gp = gpar(col = "grey"))
-
-  # Draw string
-  grid.lines(
-    x = c(unit(0, "native"), unit(1, "native")),
-    y = c(0.5, 0.5),
-    gp = gpar(col = "grey")
-  )
-
-  # Draw x-axis
-  grid.xaxis()
 
   # Draw each gene
   Genes %>%
@@ -87,53 +60,6 @@ draw_molecule <- function(Genes, Range = NULL, LayoutRow, LayoutCol) {
 
 }
 
-# Draw label for a track, in the specified row and column of the parent layout
-draw_track_label <- function(LabelText, LayoutRow, LayoutCol) {
-
-  # Create viewport for the label
-  TrackLabelViewport <- viewport(
-    layout.pos.col = LayoutCol,
-    layout.pos.row = LayoutRow,
-    just = "bottom"
-  ) %>%
-    pushViewport
-
-  # Draw border
-  grid.rect(gp = gpar(col = "grey"))
-
-  # Add label text
-  grid.text(LabelText, just = "right", x = unit(0.9, "npc"), y = unit(0.5, "npc"))
-
-  # Return to parent viewport
-  upViewport()
-
-}
-
-# Set up plot area with a grid layout
-set_up_plot_area <- function(Tracks = 1, OutputHeight, OutputWidth) {
-
-  # Set up master drawing area (basically an inset rectangle inside the output
-  # page, just to have some margins around the drawing area)
-  viewport(height = 0.8 * OutputHeight, width = 0.8 * OutputWidth) %>%
-    pushViewport
-
-  # Draw big red border around plot area
-  grid.rect(gp = gpar(col = "red"))
-
-  # Set up plot area, with grid layout
-  viewport(
-    layout = grid.layout(
-      Tracks,
-      2,
-      widths = unit(c(0.2, 0.8), "npc"),
-      heights = unit(1 / Tracks, "npc")
-    ),
-    height = unit(1, "npc"),
-    width = unit(1, "npc")
-    ) %>%
-    pushViewport
-}
-
 # Add label for a gene
 # Assumes we are already in the correct viewport
 draw_label <- function(Start, End, Label) {
@@ -153,6 +79,7 @@ Genes <- data.frame(
   End =   c(1, 12, 23, 34, 45, 56, 67, 78, 89, 100),
   Fill = rep(c("red", "blue"), 5),
   Track = rep(c("Fancypants", "Snortyhorse"), 5),
+  Molecule = rep(c("Fancypants", "Snortyhorse"), 5),
   Label = str_c("gene", 1:10)
 )
 
@@ -246,23 +173,15 @@ gene_element <- function(data = NULL, mapping = NULL) {
   # Set type
   gene_element_obj$type <- "gene"
 
-  # Handle data and mapping
-  if (! is.null(data) & is.null(mapping)) {
-    stop(
-      "Haven't yet implemented passing data and mapping directly to element",
-      call. = F
-    )
-  }
-
   # Return gene element object
   gene_element_obj
 }
 
 # Overload `+` operator to allow genemapr elements to be easily combined
-`+.genemap` <- function(x, y) {
+`+.genemap` <- function(genemap_obj, y) {
 
   # LHS should always be a genemap plot object
-  if (!identical(class(x), c("genemap"))) {
+  if (!identical(class(genemap_obj), c("genemap"))) {
     stop(
       "Initialise the genemap with ‘genemap()’ before adding elements or scales",
       call. = F
@@ -276,28 +195,163 @@ gene_element <- function(data = NULL, mapping = NULL) {
 
   # Add element
   if (identical(class(y), c("genemap", "element"))) {
-    x$elements <- c(x$elements, y)
+    genemap_obj$elements <- c(genemap_obj$elements, y)
   }
 
   # Return genemap plot object
-  x
+  genemap_obj
 }
 
-pdf("output.pdf", height = 10, width = 10)
+# Print function for genemap
+print.genemap <- function(genemap_obj) {
 
-set_up_plot_area(
-  Tracks = 3,
-  OutputHeight = unit(10, "inches"),
-  OutputWidth = unit(10, "inches")
-)
+  # To print, there must be at least one plot element
+  if (length(genemap_obj$elements) == 0) {
+    stop("This plot has no plot elements (try ‘gene_element’)", call. = F)
+  }
 
-draw_molecule(Genes %>% filter(Track == "Fancypants"), LayoutRow = 1, LayoutCol = 2)
-draw_track_label("Fancypants", LayoutRow = 1, LayoutCol = 1)
+  # New plot
+  if (!dev.cur() == 1) {
+    dev.off()
+    dev.new()
+  }
 
-draw_molecule(Genes %>% filter(Track == "Snortyhorse"), LayoutRow = 2, LayoutCol = 2)
-draw_track_label("Snortyhorse", LayoutRow = 2, LayoutCol = 1)
+  # Determine how many tracks will be drawn, and the order they will be drawn in
+  tracks <- get_aes(genemap_obj, "track") %>% unique %>% as.factor
+  tracks_n <- length(tracks)
 
-draw_molecule(Genes, LayoutRow = 3, LayoutCol = 2)
-draw_track_label("All", LayoutRow = 3, LayoutCol = 1)
+  # Set up plot area
 
-dev.off()
+  # Set up master drawing area (basically an inset rectangle inside the output
+  # page, just to have some margins around the drawing area)
+  viewport(
+    height = unit(0.8, "npc"),
+    width = unit(0.8, "npc"),
+    name = "master"
+  ) %>%
+    pushViewport
+
+  # Draw big red border around plot area
+  grid.rect(gp = gpar(col = "red"))
+
+  # Set up plot area viewport, with grid layout
+  viewport(
+    layout = grid.layout(
+      tracks_n,
+      2,
+      widths = unit(c(0.2, 0.8), "npc"),
+      heights = unit(1 / tracks_n, "npc")
+    ),
+    height = unit(1, "npc"),
+    width = unit(1, "npc"),
+    name = "plot_area"
+    ) %>%
+    pushViewport
+
+  # Draw track labels
+  for (i in 1:length(tracks)) {
+
+    # Create viewport for the track label
+    viewport(
+      layout.pos.col = 1,
+      layout.pos.row = i,
+      just = "bottom"
+    ) %>%
+      pushViewport
+
+    # Draw border
+    grid.rect(gp = gpar(col = "grey"))
+
+    # Add track label
+    grid.text(
+      tracks[i],
+      just = "right",
+      x = unit(0.9, "npc"),
+      y = unit(0.5, "npc")
+    )
+
+    # Return to parent viewport
+    upViewport()
+  }
+
+  # Draw each track
+  for (track_i in 1:tracks_n) {
+
+    # Get track name
+    track <- tracks[track_i] %>% as.character
+
+    # Determine range of all elements in this track
+    starts <- genemap_obj$data[get_aes(genemap_obj, "track") == track,][[unmap(genemap_obj)$start]]
+    ends <- genemap_obj$data[get_aes(genemap_obj, "track") == track,][[unmap(genemap_obj)$end]]
+    track_min <- min(c(starts, ends))
+    track_max <- max(c(starts, ends))
+
+    # Create viewport for this track with a native coordinate system
+    message(track)
+    message(track_i)
+    message(current.viewport())
+    viewport(
+      layout.pos.col = 2,
+      layout.pos.row = track_i,
+      xscale = c(track_min, track_max),
+      name = track
+    ) %>%
+      pushViewport
+
+  # Draw border
+  grid.rect(gp = gpar(col = "blue"))
+
+    # Draw x-axis
+    grid.xaxis()
+
+    # Get the molecules in this track
+    molecules <- genemap_obj$data[
+      get_aes(genemap_obj, "track") == track,
+      unmap(genemap_obj)$molecule
+    ]
+
+    # Emit a warning and move on if no molecules found for this track
+    if (length(molecules) == 0) {
+      warning("No molecules provided for track ", track, call. = F)
+    }
+
+    # Draw string for each molecule
+    for (molecule in molecules) {
+
+      # Determine range of molecule in this track
+      starts <- genemap_obj$data[
+        get_aes(genemap_obj, "track") == track &
+          get_aes(genemap_obj, "molecule") == molecule,][[unmap(genemap_obj)$start]]
+      ends <- genemap_obj$data[
+        get_aes(genemap_obj, "track") == track &
+          get_aes(genemap_obj, "molecule") == molecule,][[unmap(genemap_obj)$end]]
+      molecule_min <- min(c(starts, ends))
+      molecule_max <- max(c(starts, ends))
+
+      # Draw string
+      grid.lines(
+        x = unit(c(molecule_min, molecule_max), "native"),
+        y = unit(c(0.5, 0.5), "native"),
+        gp = gpar(col = "grey")
+      )
+    }
+
+  # Return to plot area viewport
+  upViewport()
+
+  }
+}
+
+# Function to get the data values for a specified aesthetic from a specified
+# genemap plot object
+get_aes <- function(genemap_obj, aesthetic) {
+  genemap_obj$data[[unmap(genemap_obj)[[aesthetic]]]]
+}
+
+# Quick access function for mappings
+unmap <- function(genemap_obj) {genemap_obj$mapping %>% as.list}
+
+# pdf("output.pdf", height = 10, width = 10)
+
+
+# dev.off()
